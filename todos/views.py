9b2ff96881c69066b2ecf0e47e7d115803dd193d
stobrien89 <stobrien@crimson.ua.edu>
2021-10-02 08:45:10 -0700

    add todo viewset

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from models import Todo
+from rest_framework import viewsets
+from rest_framework import permissions
+from serializers import TodoSerializer
 
-# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    # main query for the index route
+    query_set = Todo.objects.all()
+    # serializer class for serializing output
+    serializer_class = TodoSerializer
+    # optional permission class to set permission level
+    # could be [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
