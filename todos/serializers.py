ac6d31056b490d2848b45417132c5ff9962bf0c0
stobrien89 <stobrien@crimson.ua.edu>
2021-10-02 09:08:51 -0700

    fix typos, router tested and working

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,9 +1,9 @@
-from models import Todo
+from .models import Todo
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
 
-class Todo Serializer(serializers.HyperlinkedModelSerializer):
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Todo
         # fields that should be included in the serialized output
--- todos/views.py
+++ todos/views.py
@@ -1,12 +1,12 @@
-from models import Todo
+from .models import Todo
 from rest_framework import viewsets
 from rest_framework import permissions
-from serializers import TodoSerializer
+from .serializers import TodoSerializer
 
 
 class TodoViewSet(viewsets.ModelViewSet):
     # main query for the index route
-    query_set = Todo.objects.all()
+    queryset = Todo.objects.all()
     # serializer class for serializing output
     serializer_class = TodoSerializer
     # optional permission class to set permission level
